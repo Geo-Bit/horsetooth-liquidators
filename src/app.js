eb62589832ca6cac5db39f6e86de9c28cb4b38c9
Geo <gtip22@gmail.com>
2024-10-24 20:43:48 -0600

    user and login functionality

--- src/app.js
+++ src/app.js
@@ -1,17 +1,21 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-const productRoutes = require("./routes/productRoutes");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const productRoutes = require('./routes/productRoutes');
+const userRoutes = require('./routes/userRoutes');
 
 // Load environment variables
 dotenv.config();
 
+// Connect to the database
+connectDB();
+
 // Create app instance
 const app = express();
 
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
 // Middleware setup
 app.use(cors());
@@ -19,16 +23,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Define a test route
-app.get("/", (req, res) => {
-  res.render("index", { message: "Welcome to Horsetooth Liquidators Inc." });
+app.get('/', (req, res) => {
+  res.render('index', { message: 'Welcome to Horsetooth Liquidators Inc.' });
 });
 
-// Use product routes
-app.use("/products", productRoutes);
-app.use(express.static("public"));
+// Use product and user routes
+app.use('/products', productRoutes);
+app.use('/users', userRoutes);
+app.use(express.static('public'));
 
 // Set up server to listen on PORT
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log();
 });
+
